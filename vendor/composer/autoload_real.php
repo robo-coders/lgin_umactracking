855c9b1853bbf39198dbbc5b0548c48001b4a823
UMAC Server <umac@server.com>
2021-06-14 06:45:27 +0800

    Push From Server

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,13 +22,15 @@ class ComposerAutoloaderInit135682f5eb5cfd0c827a7a64d7770b8a
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInit135682f5eb5cfd0c827a7a64d7770b8a', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInit135682f5eb5cfd0c827a7a64d7770b8a', 'loadClassLoader'));
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
-            require_once __DIR__ . '/autoload_static.php';
+            require __DIR__ . '/autoload_static.php';
 
             call_user_func(\Composer\Autoload\ComposerStaticInit135682f5eb5cfd0c827a7a64d7770b8a::getInitializer($loader));
         } else {
