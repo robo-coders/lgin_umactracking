838e0c0755c5d391ca833ba0edf0849d23ef2d08
robo-coders <robocoders3301@gmail.com>
2021-06-22 00:16:20 +0500

    test-delte

--- app/Http/Controllers/manifestController.php
+++ app/Http/Controllers/manifestController.php
@@ -18,10 +18,6 @@ class manifestController extends Controller
         $fm = new fmRESTor("fms.umacbox.info", "uph_sys_dev", "dapi-manifest", "dapi", "webaccess@fm181");
         // $fm = new fmRESTor("uph.umacbox.info", "uph_sys", "dapi-manifest", "dapi", "webaccess@fm181");
 
-
-
-
-
         // $parameters = array(
         //     "_limit"=>5
         // );
